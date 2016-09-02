3979a3e5c58a686daa52f023473ae781961354a8
PouncingFox <nick@pouncingfoxdesign.com>
2016-09-02 09:24:38 -0500

    Proper Database Connection
    Still trying to get proper database connection

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostname = "mysql.example.com"; // the hostname you created when creating the database
-$username = "yourusername";      // the username specified when setting up the database
-$password = "yourpassword";      // the password specified when setting up the database
-$database = "databasename";      // the database name chosen when setting up the database 
+$hostname = ""; // the hostname you created when creating the database
+$username = "";      // the username specified when setting up the database
+$password = "";      // the password specified when setting up the database
+$database = "";      // the database name chosen when setting up the database 
 
 $link = mysqli_connect($hostname, $username, $password, $database);
 if (mysqli_connect_errno()) {
@@ -11,17 +11,25 @@ if (mysqli_connect_errno()) {
    exit();
 }
 
-$sql = "SELECT * FROM test_table";
-$result = mysqli_query($link,$sql) or die("Unable to select: ".mysql_error());
-print "<table>\n";
-while($row = mysqli_fetch_row($result)) {
-   print "<tr>\n";
-   foreach($row as $field) {
-       print "<td>$field</td>\n";
-   }
-   print "</tr>\n";
+function SignIn()
+{
+	session_start(); //starting the session for user profile page
+	if(!empty($_POST['user'])) //checking the 'user' name which is from Sign-In.html, is it empty or have some text
+	{ 
+		$query =  mysql_query ("SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
+		$row = mysql_fetch_array($query) or die(mysql_error()); if(!empty($row['userName']) AND !empty($row['pass']))
+		{
+			$_SESSION['userName'] = $row['pass'];
+			header($url = "Location: " . $row['defaultpage']);
+			}
+			else
+			{
+				echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY...";
+			}
 }
-print "</table>\n";
-mysqli_close($link);
-
+}
+if(isset($_POST['submit']))
+{
+	SignIn();
+}
 ?>

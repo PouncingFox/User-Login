23d85bc41bcc49ca285d7e692acc68fd27ca75d9
PouncingFox <nick@pouncingfoxdesign.com>
2016-09-02 10:29:13 -0500

    Working
    Needed Quotes around mysql_connect parameters

--- connection.php
+++ connection.php
@@ -1,31 +1,29 @@
 <?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'practice');
+define('DB_USER','root');
+define('DB_PASSWORD','');
 
-$hostname = ""; // the hostname you created when creating the database
-$username = "";      // the username specified when setting up the database
-$password = "";      // the password specified when setting up the database
-$database = "";      // the database name chosen when setting up the database 
+$con=mysql_connect() or die("Failed to connect to MySQL: " . mysql_error());
+$db=mysql_select_db("",$con) or die("Failed to connect to MySQL: " . mysql_error()); /* $ID = $_POST['user']; $Password = $_POST['pass']; */
 
-$link = mysqli_connect($hostname, $username, $password, $database);
-if (mysqli_connect_errno()) {
-   die("Connect failed: %s\n" + mysqli_connect_error());
-   exit();
-}
-
-function SignIn()
+function SignIn() 
 {
-	session_start(); //starting the session for user profile page
-	if(!empty($_POST['user'])) //checking the 'user' name which is from Sign-In.html, is it empty or have some text
+session_start(); //starting the session for user profile page
+if(!empty($_POST['user'])) //checking the 'user' name which is from Sign-In.html, is it empty or have some text
+
+{
+	$query = mysql_query("SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
+	$row = mysql_fetch_array($query) or die(mysql_error());
+	if(!empty($row['userName']) AND !empty($row['pass']))
 	{ 
-		$query =  mysql_query ("SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error());
-		$row = mysql_fetch_array($query) or die(mysql_error()); if(!empty($row['userName']) AND !empty($row['pass']))
-		{
-			$_SESSION['userName'] = $row['pass'];
-			header($url = "Location: " . $row['defaultpage']);
-			}
-			else
-			{
-				echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY...";
-			}
+		$_SESSION['userName'] = $row['pass'];
+		header($url = "Location: " . $row['defaultpage']);
+	}
+	else
+	{ 
+		echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY..."; 
+	} 
 }
 }
 if(isset($_POST['submit']))
